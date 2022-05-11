2b4a5ed1f3d51e7a034bb27356529e0271c61473
Kyle DAmato <kyledamato4@gmail.com>
2022-05-11 00:15:01 -0400

    fixed issue with direction gifs

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ move(inventory).to(0, 0)
 const character = newImage('assets/green-character/static.gif')
 
 function handleDirectionChange(direction){
-    if(direction === null) {
+    if(direction === null){
         character.src = `assets/green-character/static.gif`
     }
     if(direction === 'west'){
-        character.src = `assets/green-character/west.gif`
+        character.src = 'assets/green-character/west.gif'
     }
     if(direction === 'north'){
-        character.src = `assets/green-character/north.gif`
+        character.src = 'assets/green-character/north.gif'
     }
     if(direction === 'east'){
-        character.src = `assets/green-character/east.gif`
+        character.src = 'assets/green-character/east.gif'
     }
     if(direction === 'south'){
-        character.src = `assets/green-character/south.gif`
+        character.src = 'assets/green-character/south.gif'
     }
 }
 move(character).withArrowKeys(100, 250, handleDirectionChange)
